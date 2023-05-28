83021f541e1163656577e6896b831af1e2f8072d
Ivanovsky <ivanovsky777@gmail.com>
2023-05-27 21:23:27 -0500

    Se repara menu hamburgesa y se clasifican los archivos HTML en carpeta

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
+const nav = document.querySelector('#nav');
+const abrir = document.querySelector('#abrir');
+const cerrar = document.querySelector('#cerrar');
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
+abrir.addEventListener('click', () => {
+	nav.classList.add('visible');
+});
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+cerrar.addEventListener('click', () => {
+	nav.classList.remove('visible');
+});
